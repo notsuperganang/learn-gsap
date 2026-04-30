ec5a48a2db1c342893e20645bb07ac3e91bc027c
notsuperganang <ganangsetyohadi@gmail.com>
2026-04-30 22:45:02 +0700

    Implement gsap.to() animation in GsapTo page

--- src/pages/GsapTo.jsx
+++ src/pages/GsapTo.jsx
@@ -1,5 +1,18 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapTo = () => {
   // TODO: Implement the gsap.to() method
+  useGSAP(() => {
+    gsap.to("#blue-box", { 
+      rotation: 360,
+      x: 400,
+      duration: 3,
+      ease: 'bounce.out',
+      repeat: -1,
+      yoyo: true,
+    });
+  }, []);
 
   return (
     <main>
