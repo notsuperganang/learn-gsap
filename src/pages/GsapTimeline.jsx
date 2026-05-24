aaa41174a89fe960c35f26fb2b184e23ae6bfb15
notsuperganang <ganangsetyohadi@gmail.com>
2026-05-24 23:01:09 +0700

    feat: gsaptimeline

--- src/pages/GsapTimeline.jsx
+++ src/pages/GsapTimeline.jsx
@@ -1,5 +1,42 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapTimeline = () => {
   // TODO: Implement the gsap timeline
+  const timeline = gsap.timeline(
+    { repeat: -1, repeatDelay: 1, yoyo: true }
+  );
+  
+  useGSAP(() => {
+    timeline.to("#yellow-box", {
+      x: 250,
+      rotation: 360,
+      borderRadius: '100%',
+      duration: 3,
+      ease: 'back.inOut',
+    })
+
+    timeline.to("#yellow-box", {
+      y: 150,
+      duration: 2,
+      ease: 'back.inOut',
+    })
+
+    timeline.to("#yellow-box", {
+      y: 0,
+      duration: 2,
+      ease: 'back.inOut',
+    })
+
+    timeline.to("#yellow-box", {
+      x: 500,
+      scale: 1.5,
+      borderRadius: '20%',
+      duration: 3,
+      ease: 'back.inOut',
+      rotation: 170,
+    })
+  }, []);
 
   return (
     <main>
